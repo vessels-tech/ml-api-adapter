8e87ee3ffc38b0174e6d8fa3bd294dc462be0032
Lewis Daly <lewisdaly@me.com>
2019-05-29 12:50:35 +0800

    Working on some mysql config, dead end for now

---
+++ docker/wait_for_mysql.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# wait-for-postgres.sh
+
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+until mysql mysql -e 'select User from user' | grep central_ledger | wc -l; do
+  >&2 echo "MySQL is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "MySQL is up - executing command"
+exec $cmd
+
